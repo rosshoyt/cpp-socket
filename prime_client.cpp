10f188af8279c4f6fd8c50828bc0d8e0502a20a6
RossHoyt <rosshoytmusic@gmail.com>
2019-06-16 18:18:50 -0700

    assignment 4 submission

--- prime_client.cpp
+++ prime_client.cpp
@@ -2,9 +2,13 @@
  * Ross Hoyt
  * CPSC 5042-01, Spring 2019
  * Homework - 4
- * To compile on CS1, use:
- * g++ prime_client.cpp -o <name> -pthread -std=c++11
  *
+ * To compile on CS1, use:
+ * g++ prime_client.cpp -o <name>
+ * To run:
+ * ./<name>
+ * 
+ * Pledge:
  * I have not received unauthorized aid on this assignment.
  * I understand the answers that I have submitted. The answers submitted have not
  * been directly copied from another source, but instead
@@ -20,10 +24,7 @@
 #include <unistd.h>
 #define PORT 6543
 
-/**
- * Function which checks if a number is prime.
- * @param n - uint32_t whose primeness will be tested
- */
+// Function
 bool isPrime(uint32_t n);
 
 /**
@@ -31,77 +32,56 @@ bool isPrime(uint32_t n);
  */
 int main(int argc, char const *argv[])
 {
-    // Socket vars
-    int sock = 0;
-    struct sockaddr_in serv_addr;
-
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  printf("\nWelcome to the Prime Client Program!\n");
+  // Socket vars
+  int sock = 0;
+  struct sockaddr_in serv_addr;
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
+    printf("\n Socket creation error \n");
+    return -1;
+  }
+  memset(&serv_addr, '0', sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(PORT);
+  if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+  {
+    printf("\nInvalid address/ Address not supported \n");
+    return -1;
+  }
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+  {
+    printf("\nConnection Failed \n");
+    return -1;
+  }
+  printf("Connected to server.\n");
+  uint64_t return_status;
+  while(1 > 0)
+  {
+    uint32_t testNumber;
+    if (recv(sock, (void *)&testNumber, sizeof(uint32_t), 0) < (int)sizeof(uint32_t))
     {
-        printf("\n Socket creation error \n");
-        return -1;
+      printf("Problem receiving number from server, exiting\n");
+      return -1;
     }
-
-    memset(&serv_addr, '0', sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-    
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
-    {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
-
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
-
-    uint64_t return_status;
-    while(1 > 0)
-    {
-        uint32_t testNumber;
-        if (recv(sock, (void *)&testNumber, sizeof(uint32_t), 0) < (int)sizeof(uint32_t))
-        {		
-            printf("Problem receiving number from server, exiting\n");
-            return -1;
-        }
-        testNumber = ntohl(testNumber);
-        bool result = isPrime(testNumber);
-        
-        
-        const char* pBytesOfResult = (const char*)&result;
-
-        int lengthOfBytes = sizeof(result);
-
-        return_status = write(sock, pBytesOfResult, lengthOfBytes);
-    }
-
-    send(sock, "", 0, 0);
-    close(sock);
-    return 0;
+    testNumber = ntohl(testNumber);
+    bool result = isPrime(testNumber);
+    const char* pBytesOfResult = (const char*)&result;
+    int lengthOfBytes = sizeof(result);
+    return_status = write(sock, pBytesOfResult, lengthOfBytes);
+  }
+  send(sock, "", 0, 0);
+  close(sock);
+  return 0;
 }
 
 // Function definition
+/**
+ * Checks if a number is prime.
+ * @param n - uint32_t whose primeness will be tested
+ */
 bool isPrime(uint32_t n)
 {
-    if(n < 2) return false;
-    uint32_t divisor = 1; // (value is incremented at beginning of 'do' loop,
-    // so first tested divisor is 2)
-    // Set ceiling for largest value the prime needs to be divided by
-    uint32_t maxTestVal = (n&1) ? n / 2 + 1: n / 2;
-    // Value which indicates non-primeness when equal to 0
-    uint32_t remainder;
-    do
-    {
-        ++divisor;
-        remainder = n % divisor;
-    } while(divisor < maxTestVal && remainder != 0);
-    // Number is prime if remainder is still != 0, or edge case of n == 2 is true.
-    if(remainder != 0 || n == 2)
-    {
-        printf("Prime found %d\n", n);
-        return true;
-    }
-    return false;
+  for(size_t i = 2; i * i <= n; ++i) if (n % i == 0) return false;
+  return true;
 }
--- prime_server.cpp
+++ prime_server.cpp
@@ -5,7 +5,11 @@
  *
  * To compile on CS1, use:
  * g++ prime_server.cpp -o <name> -pthread -std=c++11
- * 
+ * To run:
+ * ./<name>
+ * To run and show primes found in the console:
+ * ./<name> show
+ *
  * Pledge:
  * I have not received unauthorized aid on this assignment.
  * I understand the answers that I have submitted. The answers submitted have not
@@ -21,192 +25,188 @@
 #include <pthread.h>
 #include <fstream>
 #include <string>
+#include <cstring>
 #define PORT 6543
 #define FILE_NAME "P.txt"
 #define MAX_VALUE UINT32_MAX
 
-// Shared variables
-uint32_t nextUnseenNumber = 2; // start search from first prime number, 2
+// Commandline input vars
+const char *SHOW_SEARCH_ARGUMENT = "show";
+bool showSearch = false; // default to not show prime search results on console
+
+// Shared vars
+uint32_t nextUnseenNumber = 2; // start search from first prime number (2)
 pthread_mutex_t numberLock, fileLock;
 std::ofstream file;
 
 // Functions
+bool isPrime(uint32_t n);
+void writeToFile(std::string str);
+uint32_t getNextUnseenNumber();
+void *findPrimes_Server(void *param);
+void *manageClientPrimeSearch(void * param);
+
+/**
+ * Server main entry point which accepts command line arguments
+ * @param *argv if "show" passed in, program displays prime search on console
+ */
+int main(int argc, char const *argv[])
+{
+  printf("\nWelcome to the Prime Server Program!\n");
+  // Check command line args
+  if(argc > 1)
+  {
+    if(std::strcmp(argv[1], SHOW_SEARCH_ARGUMENT) == 0)
+    {
+      showSearch = true;
+      printf("'Show primes discovered in console' = ON\n");
+    }
+  }
+  else printf("'Show primes discovered in console' = OFF\n"
+    "To enable this setting, pass in command 'show' when running.\n"
+    );
+  // Declare threads and locks
+  pthread_t tid_client, tid_search;
+  pthread_mutex_init(&numberLock, NULL);
+  pthread_mutex_init(&fileLock, NULL);
+  // Socket vars
+  int server_fd, new_socket;
+  struct sockaddr_in address;
+  int addrlen = sizeof(address);
+  // Create socket
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+  {
+    perror("socket failed");
+    exit(EXIT_FAILURE);
+  }
+  // Set sockaddr_in fields
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons( PORT );
+  // Forcefully attach socket to the port 8080
+  if (::bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0)
+  {
+    perror("bind failed");
+    exit(EXIT_FAILURE);
+  }
+  if (listen(server_fd, 3) < 0)
+  {
+    perror("listen");
+    exit(EXIT_FAILURE);
+  }
+  // Open the file, or create if not exists
+  file.open(FILE_NAME);
+  // Start local prime search thread
+  pthread_create(&tid_search, NULL, findPrimes_Server, NULL);
+  // Listen for client connections
+  while(1 > 0)
+  {
+    if ((new_socket = accept(server_fd, (struct sockaddr*)&address,
+                 (socklen_t *)&addrlen)) < 0)
+    {
+      perror("accept");
+      exit(EXIT_FAILURE);
+    }
+    printf("New client connected.\n");
+    pthread_create(&tid_client, NULL, manageClientPrimeSearch, (void *)new_socket);
+  }
+  return 0;
+}
+
+// Function definitions
+/**
+ * Checks if a number is prime.
+ * @param n - uint32_t whose primeness will be tested
+ */
+bool isPrime(uint32_t n)
+{
+  for(size_t i = 2; i * i <= n; ++i) if (n % i == 0) return false;
+  return true;
+}
 /**
  * Locks then writes to the shared .txt file.
  * @param str - the string to be written to the file
  */
-void writeToFile(std::string str);
+void writeToFile(std::string str)
+{
+  pthread_mutex_lock(&fileLock);
+  file << str << "\n";
+  pthread_mutex_unlock(&fileLock);
+}
 /**
- * Function which checks if a number is prime.
- * @param n - uint32_t whose primeness will be tested
- */
-bool isPrime(uint32_t n);
-/**
- * Function which copies the nextUnseenNumber, increments it in a Critical section,
- * @return the copy of nextUnseenNumber, or returns 0 once the maximum value has
+ * Copies the nextUnseenNumber, then locks and increments it
+ * @return Copy of nextUnseenNumber, or returns 0 once the maximum value has
  * been tested (indicating program completion)
  */
-uint32_t getNextUnseenNumber();
+uint32_t getNextUnseenNumber()
+{
+  // First copy of number
+  uint32_t n = nextUnseenNumber;
+  // Check if number is less than maximum test value before incrementing
+  if(nextUnseenNumber + 2 <= MAX_VALUE)
+  {
+    // Critical Section
+    pthread_mutex_lock(&numberLock);
+    // Increase nextUnseenNumber to next odd number
+    if(nextUnseenNumber & 1) nextUnseenNumber += 2; // Tests if odd number
+    else ++nextUnseenNumber; // Occurs when nextUnseenNumber is 2, at start
+    pthread_mutex_unlock(&numberLock);
+  }
+  else n = 0; // Sets the return flag of 0, which shows the program has
+  // finished testing all numbers under MAX_VALUE
+  return n;
+}
 /**
- * Multithread function used by a thread on server to find primes. Works alone or with
- * client threads to find prime numbers smaller than UINT32_MAX
- * @param param NULL
+ * Multithread function used by a thread on server to find primes. Works alone,
+ * or with client threads to find prime numbers smaller than UINT32_MAX
+ * @param NULL
  */
-void *findPrimes_Server(void *param);
+void *findPrimes_Server(void *param)
+{
+  printf("Starting server's search for prime numbers...\n");
+  uint32_t testNumber = getNextUnseenNumber(); //setting to dummy value
+  while(testNumber != 0)
+  {
+    // If number was prime, write to file
+    if(isPrime(testNumber))
+    {
+      writeToFile(std::to_string(testNumber));
+      if(showSearch) printf("Server found prime %d\n",testNumber);
+    }
+    testNumber = getNextUnseenNumber();
+  }
+  return 0;
+}
 /**
  * Multithread function which manages a client's search for primes. This passes
  * nextUnseenNumber to the client and, if prime, writes it to the shared file.
- * @param param NULL
+ * @param uint64_t client socket address
  */
-void *manageClientPrimeSearch(void * param);
-
-/**
- * Server main entry point
- */
-int main(int argc, char const *argv[])
-{
-    // Declare Threads & initialize mutex locks
-    pthread_t tid_client, tid_search;
-    pthread_mutex_init(&numberLock, NULL);
-    pthread_mutex_init(&fileLock, NULL);
-    
-    // Socket vars
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    
-    // Create socket
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-    
-    // Set sockaddr_in fields
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-    
-    // Forcefully attach socket to the port 8080
-    if (::bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0)
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    
-    // Open the file, or create if not exists
-    file.open(FILE_NAME);
-    
-    // Create local search pthread
-    pthread_create(&tid_search, NULL, findPrimes_Server, NULL);
-    
-    // Run server
-    while(1 > 0)
-    {
-        if ((new_socket = accept(server_fd, (struct sockaddr*)&address,
-                                 (socklen_t *)&addrlen)) < 0)
-        {
-            perror("accept");
-            exit(EXIT_FAILURE);
-        }
-        printf("New client connected...\n");
-        pthread_create(&tid_client, NULL, manageClientPrimeSearch, (void *)new_socket);
-        
-        
-    }
-    file.close();
-    return 0;
-}
-
-// Function definitions
-bool isPrime(uint32_t n)
-{
-    if(n < 2) return false;
-    uint32_t divisor = 1; // (value is incremented at beginning of 'do' loop,
-                          // so first tested divisor is 2)
-    // Set ceiling for largest value the prime needs to be divided by
-    uint32_t maxTestVal = (n&1) ? n / 2 + 1: n / 2;
-    // Value which indicates non-primeness when equal to 0
-    uint32_t remainder;
-    do
-    {
-        ++divisor;
-        remainder = n % divisor;
-    } while(divisor < maxTestVal && remainder != 0);
-    // Number is prime if remainder is still != 0, or edge case of n == 2 is true.
-    if(remainder != 0 || n == 2) return true;
-    return false;
-}
-void writeToFile(std::string str)
-{
-    pthread_mutex_lock(&fileLock);
-    file << str << "\n";
-    pthread_mutex_unlock(&fileLock);
-}
-uint32_t getNextUnseenNumber()
-{
-    // First create copy of the number
-    uint32_t n = nextUnseenNumber;
-    // Increase nextUnseenNumber to next odd number
-    if(nextUnseenNumber + 2 <= MAX_VALUE)
-    {
-        pthread_mutex_lock(&numberLock);
-        if(nextUnseenNumber & 1) nextUnseenNumber += 2; // Tests for odd number
-        else ++nextUnseenNumber; // Occurs when nextUnseenNumber is 2
-        pthread_mutex_unlock(&numberLock);
-    }
-    else n = 0; // Sets the return flag of 0, which shows the program has
-                // finished testing all numbers under MAX_VALUE
-    return n;
-}
-void *findPrimes_Server(void *param)
-{
-    printf("Server starting search for prime numbers...\n");
-    uint32_t testNumber = getNextUnseenNumber(); //setting to dummy value
-    while(testNumber != 0)
-    {
-        // If number was prime, write to file
-        if(isPrime(testNumber))
-        {
-            
-            writeToFile(std::to_string(testNumber));
-            printf("Server found prime %d\n",testNumber);
-        }
-        testNumber = getNextUnseenNumber();
-    }
-    return 0;
-}
 void *manageClientPrimeSearch(void *param)
 {
-    printf("Sending client integers to analyze\n");
-    uint64_t new_socket;
-    int valread;
-    new_socket = (long)param;
-    bool result;
-    uint32_t testNumber = getNextUnseenNumber();
-   
-    // Loop which continuously sends clients number to test for primeness
-    while(testNumber != 0)
+  printf("Sending client numbers to analyze...\n");
+  uint64_t new_socket;
+  int valread;
+  new_socket = (long)param;
+  bool result;
+  uint32_t testNumber = getNextUnseenNumber();
+  // Loop which continuously sends clients number to test for primality
+  while(testNumber != 0)
+  {
+    uint32_t convertedNumber = htonl(testNumber);
+    // Send client the number to test
+    write(new_socket, &convertedNumber, sizeof(convertedNumber));
+    // Read client's response
+    valread = read(new_socket, &result, sizeof(result));
+    // If tested number was prime, write to file
+    if(result)
     {
-        uint32_t convertedNumber = htonl(testNumber);
-        // Send client the number to test
-        write(new_socket, &convertedNumber, sizeof(convertedNumber));
-        // Read client's response
-        valread = read(new_socket, &result, sizeof(result));
-        // If tested number was prime, write to file
-        if(result)
-        {
-            writeToFile(std::to_string(testNumber));
-            printf("Client found prime %d\n",testNumber);
-        }
-        // Get next number for client to test
-        testNumber = getNextUnseenNumber();
+      writeToFile(std::to_string(testNumber));
+      if(showSearch) printf("Client found prime %d\n",testNumber);
     }
-    close(new_socket);
-    pthread_exit(0);
+    // Get next number for client to test
+    testNumber = getNextUnseenNumber();
+  }
+  close(new_socket);
+  pthread_exit(0);
 }
